69b49646eed68371d37a2be2b1305ee69a4c86cb
aegirorn <41763474+aegirorn@users.noreply.github.com>
2022-08-23 14:36:39 +0000

    Add project files.

---
+++ ClassLibraryDemo/ITeamLeaderAssementsService.cs
@@ -0,0 +1,24 @@
+﻿namespace ClassLibraryDemo
+{
+    /// <summary>
+    /// This is our Interface
+    /// </summary>
+    public interface ITeamLeaderAssementsService
+    {
+
+        /// <summary>
+        /// IntegrateAssesments gets data from Outsystems and bla bla bla
+        /// </summary>
+        Task IntegrateAssesments();
+        Task TryFailingInsert();
+        Task UploadAreas();
+        Task UploadAssesments();
+        Task UploadFaultyAreas();
+
+        /// <summary>
+        /// Upload Shifts does something
+        /// </summary>
+        Task UploadShifts();
+    }
+
+}
